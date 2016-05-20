7b304e59a184c3d8503edfbccabd4ec96675f9c7
David Andersson <david.andersson@alumni.chalmers.se>
2016-05-20 09:50:52 +0200

    search api done

---
+++ AjaxDemo160520/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using AjaxDemo160520.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxDemo160520.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index(string searchString)
+        {
+            SchoolContext context = new SchoolContext();
+            var matches = from m in context.Students
+                          where m.Name.ToLower().StartsWith(searchString.ToLower())
+                          select m;
+            if (matches.Count() > 0)
+                return Json(matches.First(), JsonRequestBehavior.AllowGet);
+            else
+                return Json("No matches found", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
