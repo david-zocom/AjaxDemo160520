72b9181c5cb3dc708ef64987649edf6d22c6e7e3
David Andersson <david.andersson@alumni.chalmers.se>
2016-05-20 11:29:34 +0200

    ajax

---
+++ AjaxDemo160520/Scripts/JavaScript.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+
+    $('h2').fadeIn(2000);
+
+    $('#searchField').on('input', function () {
+        $('#searchResult').text('Fetching search predictions...');
+
+        $.ajax({
+            method: 'GET',
+            url: '/Api',
+            data: {
+                searchString: $('#searchField').val()
+            },
+            dataType: 'json',
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert('Något gick fel! status:' + textStatus + "\nerror: " + errorThrown);
+            },
+            success: function (data) {
+                if (data.Name === undefined)
+                    $('#searchResult').html(data);
+                else
+                    $('#searchResult').html("Letade du efter " + data.Name + "? id=" + data.Id);
+            }
+        });
+    });//on input
+});//document.ready
